ecd0f6e2e9ce92405ee5e7b662fec055ff20e9b0
KeveloperCod <dannylatorrec@hotmail.com>
2024-08-26 00:28:10 -0500

    Proyecto de Prueba de Creative Miner Soft

---
+++ SistemaVenta.BLL/Servicios/Contrato/IDashBoardService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SistemaVenta.DTO;
+using SistemaVenta.BLL.Servicios.Contrato;
+
+namespace SistemaVenta.BLL.Servicios.Contrato
+{
+    public interface IDashBoardService
+    {
+        Task<DashBoardDTO> Resumen();
+    }
+}
